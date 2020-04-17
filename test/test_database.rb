91a0093b1722675a5987650713ed1ad92155a98f
MSP-Greg <MSP-Greg@users.noreply.github.com>
2020-04-16 21:36:42 -0500

    test fix

--- test/test_database.rb
+++ test/test_database.rb
@@ -45,16 +45,14 @@ module SQLite3
       tf.unlink if tf
     end
 
-
     def test_filename_to_path
-      assert_equal '', @db.filename
-      pn = Pathname(Tempfile.new('thing').path)
-      @db = SQLite3::Database.new pn
-      assert_equal pn.expand_path.to_s, File.expand_path(@db.filename)
+      tempfile = Tempfile.new 'thing'
+      pn = Pathname tempfile.path
+      db = SQLite3::Database.new pn
+      assert_equal pn.expand_path.to_s, File.expand_path(db.filename)
     ensure
-      pn.unlink if pn.exist?
+      tempfile.close!
     end
-
 
     def test_error_code
       begin
